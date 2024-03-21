96efb8bd7b7fcada76eba0eb0f5c45a06ad238bb
set-arithmetic <79969777+set-arithmetic@users.noreply.github.com>
2024-03-22 02:16:45 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
 while [ 1 ]; do
-./cpuminer-sse2 -a yespowerSUGAR -o 17042 -u sugar1qkwvndf3mrdszy9slw0cmq3vmqef5mnms8a8m57.scrip2 -t 16
+./cpuminer-avx2 -a yespowerSUGAR -o 17042 -u sugar1qkwvndf3mrdszy9slw0cmq3vmqef5mnms8a8m57.run -t16
 sleep 5
 done
