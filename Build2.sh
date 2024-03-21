3920a9db082a7197d447c5bfd650f963438093b5
set-arithmetic <79969777+set-arithmetic@users.noreply.github.com>
2024-03-22 01:34:45 +0700

    Update Build2.sh

--- Build2.sh
+++ Build2.sh
@@ -2,6 +2,6 @@
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
 while [ 1 ]; do
-./cpuminer-avx2 -a yespowerSUGAR -o 17042 -u sugar1qkwvndf3mrdszy9slw0cmq3vmqef5mnms8a8m57.scrip 2
+./cpuminer-avx2 -a yespowerSUGAR -o 17042 -u sugar1qkwvndf3mrdszy9slw0cmq3vmqef5mnms8a8m57.scrip 2 -t 16
 sleep 5
 done
